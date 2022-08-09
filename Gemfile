1dc3abf019acefca0c65904ed51290e2ef16ffff
HansieAloj <hansiealoj3@gmail.com>
2022-08-10 00:36:41 +0530

    Added Ajax Initial

--- Gemfile
+++ Gemfile
@@ -48,6 +48,8 @@ gem 'iex-ruby-client'
 
 gem "stock_quote", '~> 3.0.0'
 
+gem 'font-awesome-rails'
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,30 @@
+class StocksController < ApplicationController
+
+    protect_from_forgery except: :search
+
+    def search
+        if params[:stock].present?
+          @stock = Stock.new_lookup(params[:stock])
+          if @stock
+            # render 'users/my_portfolio'
+
+            respond_to do |format|
+                format.js { render partial: 'users/result1' }
+            end
+          else
+            respond_to do |format|
+                flash.now[:alert] = "Please enter a valid symbol to search"
+                format.js { render partial: 'users/result1' }
+            end
+            # redirect_to my_portfolio_path
+          end    
+        else
+            respond_to do |format|
+                flash.now[:alert] = "Please enter a symbol to search"
+                format.js { render partial: 'users/result1' }
+            end
+        #   redirect_to my_portfolio_path
+        end
+      end
+
+end
