85ccfadffed6f1f5f4f305bd94111e4aed52890a
HansieAloj <hansiealoj3@gmail.com>
2022-08-10 05:33:21 +0530

    Added Stock Listing

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -6,11 +6,11 @@ class StocksController < ApplicationController
         if params[:stock].present?
           @stock = Stock.new_lookup(params[:stock])
           if @stock
-            # render 'users/my_portfolio'
+            render 'users/my_portfolio'
 
-            respond_to do |format|
-                format.js { render partial: 'users/result1' }
-            end
+            # respond_to do |format|
+            #     format.js { render partial: 'users/result1' }
+            # end
           else
             respond_to do |format|
                 flash.now[:alert] = "Please enter a valid symbol to search"
